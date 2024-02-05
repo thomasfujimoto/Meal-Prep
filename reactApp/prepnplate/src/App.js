05a96a7fb9d8488ac35337dc006927b47e66a046
thomas fujimoto <fujimotothomas@outlook.com>
2024-02-04 17:36:30 -0800

    Add: ReactJS files for website

---
+++ reactApp/prepnplate/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './images/prepandplate1.png';
+import './App.css'; 
+
+const App = () => {
+  return (
+    <div className="center-container">
+      <img src={logo} alt="Logo" className="logo" />
+    </div>
+  );
+};
+
+
+export default App;
