59c825dcf299108587a6ac29df769fc38ef41653
whelweh <130249465+whelweh@users.noreply.github.com>
2024-02-11 21:38:55 +0000

    initial backend

--- Prep_N_Plate_Backend/main.py
+++ Prep_N_Plate_Backend/main.py
@@ -1,4 +1,15 @@
 import pandas as pd
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+import json
+from django.urls import path
+from .views import receive_data
+
+#Django receive setup
+urlpatterns = [
+    path('api/data/', receive_data, name='receive_data'),
+]
 
 #Categories in the .CSV:
 # 22 minute meal
@@ -20,9 +31,25 @@ df = df.dropna(axis=0, subset='calories')
 #Print first 5 rows for the first 5 columns
 print(df[df.columns[:5]].head()) 
 
+#Receives the int array which represents survey choices selected or not
+@csrf_exempt
+@required_http_methods(["POST"])
+def receive_int_data(request):
+
+#receives the string array which represents the chosen recipes selected after survey 
+@csrf_exempt
+@required_http_methods(["POST"])
+def receive_str_data(request):
+
+#receives recipe string name user chose to get more info about
+@csrf_exempt
+@required_http_methods(["POST"])
+def receive_strls_data(request):
+
+
 #return recipes based on user input from survey inputs above, 
 #1 = choice selected, 0 not selected
-SurveyInput(array of ints):
+def SurveyInput(array of ints):
     #order of array indexes: vegetarian, vegan, treeNutFree, sugarConscious, threeIngredients, cakeWeek, tTMinute, wasteless
     #Base cases: Nothing selected, drop nothing
     #Something selected: Filter out everything not selected
@@ -30,10 +57,10 @@ SurveyInput(array of ints):
      
 
 #takes an array of recipe names chosen in the previous frontend step and then creates a grocery list 
-GenerateUserGroceryList(array of recipe names):
+def GenerateUserGroceryList(array of recipe names):
 
 #takes an array of recipe names chosen in the previous frontend step and then creates a schedule with recipe directions
-GenerateUserSchedule(array of recipe names):
+def GenerateUserSchedule(array of recipe names):
 
 #takes recipe string name and returns calore info, ingredients, and directions
-RecipeInfo(recipe string name):
+def RecipeInfo(recipe string name):
