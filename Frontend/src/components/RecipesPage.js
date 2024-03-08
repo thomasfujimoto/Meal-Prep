8da48eca13063ee454b61ec764d0a816692f2876
mtan42 <mtan42@ucsc.edu>
2024-03-08 12:25:25 -0800

    update

--- Frontend/src/components/RecipesPage.js
+++ Frontend/src/components/RecipesPage.js
@@ -1,116 +1,128 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
 import '../styles/RecipesPage.css';
 import logo from '../images/prepandplate1.png'
+//import html2canvas from 'html2canvas'
+//import jsPDF from 'jspdf'
 
-/*
+
 const RecipesPage = () => {
   // Example recipe data (you can replace this with your actual recipe data)
-  const recipe = {
-    id: 1,
-    title: 'Spaghetti Carbonara',
-    description: 'A classic Italian pasta dish with bacon, eggs, and cheese.',
-    steps: [
-      'Cook spaghetti according to package instructions.',
-      'In a skillet, cook bacon until crispy. Remove bacon and set aside. Keep the bacon fat in the skillet.',
-      'In a bowl, whisk together eggs, grated cheese, salt, and pepper.',
-      'Add cooked spaghetti to the skillet with bacon fat. Toss to coat the spaghetti in the fat.',
-      'Remove skillet from heat. Quickly add the egg and cheese mixture to the spaghetti, stirring continuously to coat the spaghetti.',
-      'Crumble the cooked bacon and add it to the skillet. Stir to combine. Serve immediately.'
-    ]
+  const recipeUrl = 'http://localhost:5000/RecipesPage'
+
+  const leftLink = "/GroceryList"
+  const rightLink = "/PlannerPage"
+
+  const fakeRecipe = [ {id: "1", name:'Cook spaghetti according to package instructions.'},
+    {id: "2", name:  'In a skillet, cook bacon until crispy. Remove bacon and set aside. Keep the bacon fat in the skillet.'},
+    {id: "3", name:  'In a bowl, whisk together eggs, grated cheese, salt, and pepper.'},
+    {id: "4", name:  'Add cooked spaghetti to the skillet with bacon fat. Toss to coat the spaghetti in the fat.'},
+    {id: "5", name:  'Remove skillet from heat. Quickly add the egg and cheese mixture to the spaghetti, stirring continuously to coat the spaghetti.'},
+    {id: "6", name:  'Crumble the cooked bacon and add it to the skillet. Stir to combine. Serve immediately.'}
+  ]
+
+  async function getRecipe(){
+    try {
+      const response = await fetch (recipeUrl, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error)
+      return fakeRecipe;
+    }
+  }
+
+  const fetchRecipe = async () => {
+    /*
+    const g = localStorage.getItem("RecipePage")
+    if (g) {
+      setRecipe(JSON.parse(g))
+      return;
+    } else {
+      const data = await getRecipe();
+      setRecipe(data);
+    }
+    */
+    const data = await getRecipe();
+    setRecipe(data);
   };
 
+  const [recipeSteps, setRecipe] = useState([]);
+
+/*
+  const pdfRef = useRef()
+
   const genPdf = () => {
-
+    const input = pdfRef.current;
+    html2canvas(input).then( canvas => {
+      const imgData = canvas.toDataURL("image/png")
+      const pdf = jsPDF("p", "mm", "letter", true)
+      const width = pdf.internal.pageSize.getWidth()
+      const height = pdf.internal.pageSize.getHeight()
+      const imgWidth = canvas.width
+      const imgHeight = canvas.height
+      const ratio = Math.min(width/imgWidth, height/imgHeight)
+      const imageX = (width - imgWidth*ratio) / 2
+      const imageY = 30
+      pdf.addImage(imgData, 'PNG', imageX, imageY, imgWidth*ratio, imgHeight*ratio)
+      pdf.save('recipe.pdf')
+    })
   }
-
-  return (
-    <div className='top-container'>
-    <div  className='hor-container'>
-      <div className='hor-item hor-left'><button onClick={genPdf}>PDF</button></div>
-      <div className='hor-item hor-mid title'><center>Recipe</center></div>
-      <div className='hor-item hor-right'><img width='100' src={logo} alt="Logo"/></div>
-    </div>
-
-    //Display recipe steps
-    <div className="recipe-steps">
-        <ol>
-          {recipe.steps.map((step, index) => (
-          <p key={index}><strong>Step {index + 1}:</strong> {step}</p>
-          ))}
-        </ol>
-      </div>
-
-    <div className='hor-container'>
-      <div className='hor-item hor-left'><a className='back button' href="#/GroceryList">&lt; Grocery List</a></div>
-      <div className='hor-item hor-right'><a className='next button' href="#/PlannerPage">Weekly Planner &gt;</a></div>
-      
-    </div>
-  </div>
- )
-}  
 */
-
-//{Backend Integration} (Should work)
-const RecipesPage = () => {
-  const [recipe, setRecipe] = useState(null);
-  const [loading, setLoading] = useState(true);
+  
 
   useEffect(() => {
-    const fetchRecipe = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/submit-recipe');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        setRecipe(data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    };
-
-    fetchRecipe();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!recipe) {
-    return <div>Error: Recipe not found</div>;
-  }
-
-  const genPdf = () => {
-
-  }
+    fetchRecipe()
+  }, [])
 
   return (
-    <div className='top-container'>
-    <div  className='hor-container'>
-      <div className='hor-item hor-left'><button onClick={genPdf}>PDF</button></div>
-      <div className='hor-item hor-mid title'><center>Recipe</center></div>
-      <div className='hor-item hor-right'><img width='100' src={logo} alt="Logo"/></div>
-    </div>
-
-    //Display recipe steps
-    <div className="recipe-steps">
-        <ol>
-          {recipe.steps.map((step, index) => (
-          <p key={index}><strong>Step {index + 1}:</strong> {step}</p>
-          ))}
-        </ol>
-      </div>
-
-    <div className='hor-container'>
-      <div className='hor-item hor-left'><a className='back button' href="#/GroceryList">&lt; Grocery List</a></div>
-      <div className='hor-item hor-right'><a className='next button' href="#/PlannerPage">Weekly Planner &gt;</a></div>
+    <>
       
-    </div>
-  </div>
- )
-}  
+      <div className='center-container'>
+        
+          <div  className='hor-container'>
+            <div className='hor-item hor-mid title'><center>Recipe</center></div>
+            <div className='hor-item hor-right'><img width='100' src={logo} alt="Logo"/></div>
+          </div>
+          <div className='hor-item hor-mid'>
+            {recipeSteps.map((item, index) => (            
+              <div key={item._id}>
+                <p>{index+1}. {item.name}</p>
+              </div>
+            ))}
+
+          </div>
+        
+        <div className='hor-container'>
+          <BackButton to={leftLink} text="Grocery List" />
+          <ForwardButton to={rightLink} text="Weekly Planner" />
+        </div>
+      </div>
+    </>
+  )
+}
+
+const BackButton = ({ to, text }) => {
+  return (
+    <Link to={to} className="back button">
+      {text}
+    </Link>
+  );
+};
+
+const ForwardButton = ({ to, text }) => {
+  return (
+    <Link to={to} className="next button">
+      {text}
+    </Link>
+  );
+};
+
 export default RecipesPage;
