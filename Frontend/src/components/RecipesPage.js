559bbfecc919d632dec8422f25f946b37312f43f
mtan42 <mtan42@ucsc.edu>
2024-03-08 17:23:37 -0800

    recipe update

--- Frontend/src/components/RecipesPage.js
+++ Frontend/src/components/RecipesPage.js
@@ -1,117 +1,133 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { PDFDocument, rgb } from 'pdf-lib'; // Import necessary functions from pdf-lib
 import '../App.css';
 import '../styles/RecipesPage.css';
 import logo from '../images/prepandplate1.png'
-//import html2canvas from 'html2canvas'
-//import jsPDF from 'jspdf'
-
 
 const RecipesPage = () => {
-  // Example recipe data (you can replace this with your actual recipe data)
-  const recipeUrl = 'http://localhost:5000/RecipesPage'
-
-  const leftLink = "/GroceryList"
-  const rightLink = "/PlannerPage"
-
-  const fakeRecipe = [ {id: "1", name:'Cook spaghetti according to package instructions.'},
-    {id: "2", name:  'In a skillet, cook bacon until crispy. Remove bacon and set aside. Keep the bacon fat in the skillet.'},
-    {id: "3", name:  'In a bowl, whisk together eggs, grated cheese, salt, and pepper.'},
-    {id: "4", name:  'Add cooked spaghetti to the skillet with bacon fat. Toss to coat the spaghetti in the fat.'},
-    {id: "5", name:  'Remove skillet from heat. Quickly add the egg and cheese mixture to the spaghetti, stirring continuously to coat the spaghetti.'},
-    {id: "6", name:  'Crumble the cooked bacon and add it to the skillet. Stir to combine. Serve immediately.'}
-  ]
-
-  async function getRecipe(){
-    try {
-      const response = await fetch (recipeUrl, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error)
-      return fakeRecipe;
-    }
-  }
-
-  const fetchRecipe = async () => {
-    /*
-    const g = localStorage.getItem("RecipePage")
-    if (g) {
-      setRecipe(JSON.parse(g))
-      return;
-    } else {
-      const data = await getRecipe();
-      setRecipe(data);
-    }
-    */
-    const data = await getRecipe();
-    setRecipe(data);
-  };
-
-  const [recipeSteps, setRecipe] = useState([]);
-
-/*
-  const pdfRef = useRef()
-
-  const genPdf = () => {
-    const input = pdfRef.current;
-    html2canvas(input).then( canvas => {
-      const imgData = canvas.toDataURL("image/png")
-      const pdf = jsPDF("p", "mm", "letter", true)
-      const width = pdf.internal.pageSize.getWidth()
-      const height = pdf.internal.pageSize.getHeight()
-      const imgWidth = canvas.width
-      const imgHeight = canvas.height
-      const ratio = Math.min(width/imgWidth, height/imgHeight)
-      const imageX = (width - imgWidth*ratio) / 2
-      const imageY = 30
-      pdf.addImage(imgData, 'PNG', imageX, imageY, imgWidth*ratio, imgHeight*ratio)
-      pdf.save('recipe.pdf')
-    })
-  }
-*/
   
 
+  //Fake recipe, replace if needed
+  const fakeRecipe = {
+    id: 1,
+    steps: [
+      'Using an electric mixer on medium-high speed, beat butter, sugar, orange zest, salt, and cardamom, if using, in a large bowl until fluffy.',
+      'Pour 3 Tbsp. warm water (105°F–115°F) into a small bowl. Add yeast and 1 Tbsp. sugar and whisk to combine. Let sit until foamy, about 10 minutes.',
+      'Using electric mixer on medium-high speed, beat egg yolk, orange juice, oil, salt, and remaining 1 Tbsp. sugar in a large bowl. Add 1 2/3 cups flour and yeast mixture. Beat until dough just comes together. Turn out dough onto a lightly floured surface. Knead several times with floured hands until smooth, about 5 minutes (dough will be sticky).',
+      'Spray a clean large bowl with nonstick spray. Place dough in bowl and turn to coat. Cover with a towel and let sit in a warm place until doubled in size, about 45 minutes.'
+    ]
+  };
+
+
+  const [recipeSteps, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  //Get recipe from backend
   useEffect(() => {
-    fetchRecipe()
-  }, [])
+    const fetchRecipe = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/RecipesPage');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const data = await response.json();
+        setRecipe(data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
 
-  return (
-    <>
-      
-      <div className='center-container'>
-        
-          <div  className='hor-container'>
-            <div className='hor-item hor-mid title'><center>Recipe</center></div>
-            <div className='hor-item hor-right'><img width='100' src={logo} alt="Logo"/></div>
-          </div>
-          <div className='hor-item hor-mid'>
-            {recipeSteps.map((item, index) => (            
-              <div key={item._id}>
-                <p>{index+1}. {item.name}</p>
-              </div>
-            ))}
+    fetchRecipe();
+  }, []);
 
-          </div>
-        
-        <div className='hor-container'>
-          <BackButton to={leftLink} text="Grocery List" />
-          <ForwardButton to={rightLink} text="Weekly Planner" />
+
+  // Function to handle PDF download
+  const handleDownloadPDF = async () => {
+    const pdfDoc = await PDFDocument.create();
+    const page = pdfDoc.addPage();
+    const { width, height } = page.getSize();
+
+    const stepY = height - 150;
+    fakeRecipe.steps.forEach((step, index) => {          //replace with fakeRecipe with recipeSteps once done testing
+      page.drawText(`Step ${index + 1}: ${step}`, {
+        x: 50,
+        y: stepY - index * 20,
+        size: 12,
+      });
+    });
+
+    const pdfBytes = await pdfDoc.save();
+    downloadPdf(pdfBytes, 'recipe.pdf');
+  };
+
+  const downloadPdf = (data, filename) => {
+    const blob = new Blob([data], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(a);
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+
+  // Return fakeRecipe as fallback when recipeSteps is null, used for testing purposes
+  if (!recipeSteps) {
+    return (
+      <div>
+        <div className="header">
+          <button onClick={handleDownloadPDF} className="pdf-button">Download PDF</button>
+          <div className='title'>RECIPE</div>
+          <img src={logo} alt="Prep and Plate 1" className="recipe-image" />
         </div>
+        <div className="recipe-steps">
+          {fakeRecipe.steps.map((step, index) => (
+            <p key={index} className ="recipe-step"><strong className="large-font">Step {index + 1}:</strong> <span className="large-font" style={{ color: '#006400' }}>{step}</span></p>
+
+          ))}
+
+        </div>
+        <BackButton to="/GroceryList" text="<- Grocery List" />
+        <ForwardButton to="/PlannerPage" text="Weekly Planner ->" />
       </div>
-    </>
-  )
-}
+    );
+  }
+
+
+  //Return Recipe if successful/not null
+  return (
+    <div>
+      <div className="header">
+        <button onClick={handleDownloadPDF} className="pdf-button">Download PDF</button>
+        <div className='title'>RECIPE</div>
+        <img src={logo} alt="Prep and Plate 1" className="recipe-image" />
+      </div>
+      <div className="recipe-steps">
+        {recipeSteps.steps.map((step, index) => (
+          <p key={index} className ="recipe-step"><strong className="large-font">Step {index + 1}:</strong> <span className="large-font" style={{ color: '#006400' }}>{step}</span></p>
+
+        ))}
+
+      </div>
+      <BackButton to="/GroceryList" text="<- Grocery List" />
+      <ForwardButton to="/PlannerPage" text="Weekly Planner ->" />
+    </div>
+  );
+};
 
 const BackButton = ({ to, text }) => {
   return (
-    <Link to={to} className="back button">
+    <Link to={to} className='back button'>
       {text}
     </Link>
   );
